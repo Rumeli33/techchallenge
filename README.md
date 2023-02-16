7dd3b9fdffa56608f2ca763581d3bf8c51b41e9c
Rumeli Paul <rumelipaul@gmail.com>
2023-02-16 19:27:08 +0100

    README added

---
+++ README.md
@@ -0,0 +1,14 @@
+
+## Tech challenge for Ironhack, Server side .
+
+- it has a  REST API created on the server side.
+- this file contains the code for API endpoints to get the list of all phones.
+-  end point for When a phone model is selected from the list, a detailed view of the phone is shown with a few specific details
+
+
+
+
+## 🔗 Links to Our Repos
+[https://github.com/tech-challenge/techchallenge-client]
+
+[https://github.com/tech-challenge/techchallenge]
--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const data = require ('./data/phones.json');
+const cors = require('cors')
 
+ // Use this after the variable declaration
 // We create our own server named app
 // Express server will be handling requests and responses
 const app = express();
-// our first Route
-app.get('/phones', (request, response, next) => {
-    
-    response.json(data);
-  });
-
-  app.get('/phones/:id', (request, response, next) => {
+app.use(cors())
+app.get('/phones/:id', (request, response, next) => {
     //console.log(data);
     const paramid = request.params.id;
     console.log(paramid);
@@ -19,5 +16,12 @@ app.get('/phones', (request, response, next) => {
     response.json(onePhone);
   });
 
+// our first Route
+app.get('/phones', (request, response, next) => {
+    
+    response.json(data);
+  });
+
+ 
   // Server Started
 app.listen(3000, () => console.log('My first app listening on port 3000! '));
